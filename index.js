75a1785a3b41e37ac7d56f1347c6b5105ddebc93
samandargofurov <samandargofurov236@gmail.com>
2024-01-06 22:01:59 +0500

    11-lesson

---
+++ index.js
@@ -0,0 +1,84 @@
+// 1-masala
+
+// let arrStr = "hello world";
+
+// function reverseWord(str) {
+//     let strArr = str.split("");
+//     let teskariArr = strArr.reverse();
+//     let teskariStr = teskariArr.join("");
+//     return teskariStr;
+// }
+
+
+// 2-masala
+
+// function uzunSozniTopish(str) {
+//     let sozlar = str.split(" ");
+//     let uzunSoz = "";
+//     for (let i = 0; i < sozlar.length; i++) {
+//       if (sozlar[i].length > uzunSoz.length) {
+//         uzunSoz = sozlar;
+//       }
+//     }
+//     return uzunSoz;
+//   }
+
+
+// 3-masala
+
+// function unlilarSoni(str) {
+//     let counter = 0;
+//     let kichikSatr = satr.toLowerCase();
+//     for (let chart of str) {
+//       if (kichikSatr[i] == 'a' || kichikSatr[i] == 'i' || kichikSatr[i] == 'u' || kichikSatr[i] == 'o' || kichikSatr[i] == 'e') {
+//         counter++;
+//       }
+//     }
+//     return counter;
+//   }
+
+
+// 4-masala
+
+// function palindromChecked(arg) {
+//     let reverse = reverseString(arg);
+//     if (arg == reverse) {
+//         return true
+//     } else{
+//         return false
+//     }
+// }
+
+
+// 5-masala
+
+
+
+// 6-masala
+
+
+// function boshHarfBilan(arg) {
+//     let sozlar = arg.split();
+//     let boshHarf = [];
+//     for (const soz of sozlar) {
+//         let yangiSoz = soz[0].toUpperCase() + soz.slice(1);
+//         boshHarf.push(yangiSoz);
+//     }
+//     let res = boshHarf.join(" ");
+//     return res
+// }
+
+
+// 7-masala
+
+// 8-masala
+
+// 9-masala
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [6, 7, 8, 9, 10];
+// let arr3 = arr1.concat(arr2)
+// console.log(arr3);
+
+
+// 10-masala
